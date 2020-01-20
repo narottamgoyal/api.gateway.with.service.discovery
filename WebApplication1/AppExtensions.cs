34e5586540fd996d94046651cc99a151058c54ae
narottam <NAROTTAMGOYAL66@GMAIL.COM>
2020-01-21 00:08:42 +0530

    added inital code

---
+++ WebApplication1/AppExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public static class AppExtensions
+    {
+        public static IServiceCollection AddConsulConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
+            {
+                var address = configuration.GetValue<string>("Consul:Host");
+                consulConfig.Address = new Uri(address);
+            }));
+            return services;
+        }
+
+        public static IApplicationBuilder UseConsul(this IApplicationBuilder app)
+        {
+            var consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
+            var logger = app.ApplicationServices.GetRequiredService<ILoggerFactory>().CreateLogger("AppExtensions");
+            var lifetime = app.ApplicationServices.GetRequiredService<IApplicationLifetime>();
+
+            //if (!(app.Properties["server.Features"] is FeatureCollection features)) return app;
+
+            //var addresses = features.Get<IServerAddressesFeature>();
+            //var address = addresses.Addresses.First();
+
+            //Console.WriteLine($"address={address}");
+
+            //var uri = new Uri(address);
+            var registration = new AgentServiceRegistration()
+            {
+                ID = $"MyService-57084", //{uri.Port}",
+                // servie name  
+                Name = "WebApplication1",
+                Address = "localhost", //$"{uri.Host}",
+                Port = 57084  // uri.Port
+            };
+
+            logger.LogInformation("Registering with Consul");
+            consulClient.Agent.ServiceDeregister(registration.ID).ConfigureAwait(true);
+            consulClient.Agent.ServiceRegister(registration).ConfigureAwait(true);
+
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                logger.LogInformation("Unregistering from Consul");
+                consulClient.Agent.ServiceDeregister(registration.ID).ConfigureAwait(true);
+            });
+
+            return app;
+        }
+    }
+}
